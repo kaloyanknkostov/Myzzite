5a766075f4876f1d773e77c51657674539626320
kaloyanknkostov <kaloyanknkostov@gmail.com>
2026-08-12 23:02:31 +0300

    fixes

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,45 +5,35 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-# --- Enable COPR Repositories ---
-dnf5 -y copr enable imput/helium 
+dnf5 -y copr enable  imput/helium 
 dnf5 -y copr enable scottames/ghostty
 dnf5 -y copr enable atim/starship 
 dnf5 -y copr enable lihaohong/yazi
 dnf5 -y copr enable lionheartp/Hyprland  
 dnf5 -y copr enable theblackdon/kineticwe  
-dnf5 -y copr enable codifryed/CoolerControl
-
-# --- Install Packages ---
-dnf5 install -y \
-  tmux neovim ripgrep helium-bin dnf-plugins-core \
-  atuin bat coolercontrol docker eza ghostty postgresql \
-  postgresql-server starship stow yazi zoxide zsh
-
-# --- Desktop Component Swap & Clean ---
-# Uses explicit remove + install steps with --skip-unavailable to prevent build failures
-dnf5 remove -y --skip-unavailable kwin kwin-common kwin-libs kglobalacceld kdecoration
-dnf5 install -y kineticwe
+#____packages
+dnf5 install -y tmux neovim ripgrep helium-bin
+dnf5 install -y atuin bat coolercontrol docker eza ghostty postgresql postgresql-server starship stow yazi zoxide zsh 
+dnf5 install -y dnf-plugins-core
+#____
+dnf5 swap -y kwin kineticwe  
+dnf5 remove -y kwin-common kwin-libs kglobalacceld kdecoration
 dnf5 install -y noctalia-git
-
-# --- Disable COPR Repositories ---
+#____
+#repos disabled
 dnf5 -y copr disable imput/helium 
 dnf5 -y copr disable scottames/ghostty
 dnf5 -y copr disable atim/starship 
 dnf5 -y copr disable lihaohong/yazi
 dnf5 -y copr disable lionheartp/Hyprland  
-dnf5 -y copr disable theblackdon/kineticwe  
-dnf5 -y copr disable codifryed/CoolerControl
-
-# --- Cleanup & Services ---
+dnf5 -y copr disable theblackdon/kineticwe
 dnf5 clean all
 systemctl enable podman.socket
 systemctl enable coolercontrold
+#cursor
+#curl https://cursor.com/install -fsS | bash
+# opencode
+#curl -fsSL https://opencode.ai/install | bash
+# sdkman 
+#curl -s "https://get.sdkman.io" | bash
 
-# --- External Tool Installers ---
-# Cursor
-curl https://cursor.com/install -fsS | bash
-# OpenCode
-curl -fsSL https://opencode.ai/install | bash
-# SDKMAN!
-curl -s "https://get.sdkman.io" | bash
