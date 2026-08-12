5bd9145e17587fd7f1d819e28d617a153cb13d21
kaloyanknkostov <kaloyanknkostov@gmail.com>
2026-08-12 18:14:06 +0300

    more fixes

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,16 +6,16 @@ set -ouex pipefail
 cp -avf "/ctx/system_files"/. /
 
 dnf5 copr enable -y imput/helium 
-dnf5 -y copr enable codelingo/coolercontrol 
-dnf5 -y copr enable pgdev/ghostty
+dnf5 -y copr enable scottames/ghostty
 dnf5 -y copr enable atim/starship 
-dnf5 -y copr enable varlad/yazi
-dnf5 copr enable -y lionheartp/Hyprland  
-dnf5 copr enable -y theblackdon/kineticwe  
+dnf5 -y copr enable lihaohong/yazi
+dnf5 -y copr enable lionheartp/Hyprland  
+dnf5 -y copr enable theblackdon/kineticwe  
+dnf5 -y copr enable codifryed/CoolerControl
 #____packages
 dnf5 install -y tmux neovim ripgrep helium-bin
 dnf5 install -y atuin bat coolercontrol docker eza fzf ghostty gradle maven postgresql postgresql-server starship stow yazi zoxide zsh 
-
+dnf5 install dnf-plugins-core
 #____
 dnf5 swap -y kwin kineticwe  
 dnf5 remove -y kwin-common kwin-libs kglobalacceld kdecoration
@@ -23,7 +23,6 @@ dnf5 install -y noctalia-git
 #____
 #repos disabled
 dnf5 -y copr disable imput/helium 
-dnf5 -y copr disable atim/atuin
 dnf5 -y copr disable codelingo/coolercontrol 
 dnf5 -y copr disable pgdev/ghostty
 dnf5 -y copr disable atim/starship 
@@ -33,7 +32,7 @@ dnf5 -y copr disable theblackdon/kineticwe
 
 dnf5 clean all
 systemctl enable podman.socket
-systemctl enable coolercontrold.service
+systemctl enable coolercontrold
 #cursor
 curl https://cursor.com/install -fsS | bash
 # opencode
